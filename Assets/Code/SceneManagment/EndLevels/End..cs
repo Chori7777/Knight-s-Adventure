f13637cbd4c93350cee9f0a4cfac8ae8ef27b4b7
Chori7777 <thiagomeza9876@gmail.com>
2025-10-24 19:29:40 -0300

    Pre-alfa1.4.9
    Bocetos mundo 2 y 3
    CAMBIOS ANTES DE REWORKEAR EL FUNCIONAMIENTO DE ENEMIGOS

--- Assets/Code/SceneManagment/EndLevels/End..cs
+++ Assets/Code/SceneManagment/EndLevels/End..cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class End : MonoBehaviour
 {
+    [SerializeField] private string sceneToLoad; // <- Elegís el nombre en el Inspector
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-        { 
-        changeScene("TheForest");
+        {
+            changeScene(sceneToLoad);
         }
     }
+
     public void changeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
